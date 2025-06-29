e62e4a2ba3ddbb50bbf189fd23b09cbfd87a692f
Deepika5reddy <reddy5deeps@gmail.com>
2025-06-29 14:53:38 -0500

    changed the output sequence

--- arry_rotation.js
+++ arry_rotation.js
@@ -13,14 +13,14 @@ const length = arr.length;
     console.log('Enter valid position to shift');
     return;
   }
-  console.log("Original Array:"+arr);
-  console.log("Length of array:" +length);
-  console.log('How many position to rotate:'+position);
   const rotation = position % arr.length; // gives the rotating times 2 % 8 = 2
   // arr.slice(rotation) gives the part after the rotated section [3,4,5,6,7,8].
   // arr.slice(0,rotation) gets the rotated-out part.
   //.concat joins both
   const rotated_array = arr.slice(rotation).concat(arr.slice(0,rotation))
+  console.log("Original Array:"+arr);
+  console.log("Length of array:" +length);
+  console.log('How many position to rotate:'+position);
   console.log ('Rotated Array :' +rotated_array);
   return rotated_array;
 }
